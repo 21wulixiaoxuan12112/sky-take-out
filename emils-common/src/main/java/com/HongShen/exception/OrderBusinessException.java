8a840f93c71593162a71dfd286b9b47de0bf9c63
Administrator <1846623298@qq.com>
2023-12-20 15:13:02 +0800

    COMMIT CODE

---
+++ emils-common/src/main/java/com/HongShen/exception/OrderBusinessException.java
@@ -0,0 +1,9 @@
+package com.HongShen.exception;
+
+public class OrderBusinessException extends BaseException {
+
+    public OrderBusinessException(String msg) {
+        super(msg);
+    }
+
+}
---
+++ emils-server/src/main/java/com/HongShen/mapper/EmilsMapper.java
@@ -0,0 +1,20 @@
+package com.HongShen.mapper;
+
+import com.HongShen.entity.Emils;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zy
+ * @date 2023/12/20 14:22
+ */
+@Mapper
+public interface EmilsMapper {
+    /*
+     * 新增邮箱
+     * */
+    @Insert("insert into employee (mailUser,mailPassword,state,create_time, update_time) " +
+            "values " +
+            "(#{mailUser},#{mailPassword},#{state},#{createTime},#{updateTime})")
+    void insert(Emils emils);
+}
