879fea1cf456ea07f93524916b2699602964a76a
Administrator <1846623298@qq.com>
2023-12-20 15:03:51 +0800

    COMMIT CODE

---
+++ emils-pojo/src/main/java/com/sky/vo/EmilsVO.java
@@ -0,0 +1,43 @@
+package com.sky.vo;
+
+/**
+ * @author zy
+ * @date 2023/12/20 12:34
+ */
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(description = "邮箱返回的数据格式")
+public class EmilsVO implements Serializable {
+
+    @ApiModelProperty("主键值")
+    private Integer id;
+
+    @ApiModelProperty("邮箱名")
+    private String mailUser;
+
+    @ApiModelProperty("密码")
+    private String mailPassword;
+
+    @ApiModelProperty("邮箱状态")
+    private Integer state;
+
+    @ApiModelProperty("创建时间")
+    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    @ApiModelProperty("修改时间")
+    private LocalDateTime updateTime;
+}
---
+++ emils-server/src/main/java/com/sky/controller/admin/EmilsController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.admin;
+
+/**
+ * @author zy
+ * @date 2023/12/20 14:19
+ */
+
+import com.sky.dto.EmilsDTO;
+import com.sky.dto.EmployeeDTO;
+import com.sky.result.Result;
+import com.sky.service.EmilsService;
+import com.sky.service.EmployeeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 员工管理
+ */
+@RestController
+@RequestMapping("/admin/emils")
+@Slf4j
+@Api(tags = "邮箱相关接口")
+public class EmilsController {
+
+    @Autowired
+    private EmilsService emilsService;
+
+    /**
+     * 新增员工
+     * @param emilsDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增邮箱")
+    public Result save(@RequestBody EmilsDTO emilsDTO){
+        log.info("新增员工：{}",emilsDTO);
+        emilsService.save(emilsDTO);
+        return Result.success();
+    }
+}
